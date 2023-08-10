4dbf9c90eaf01469e241c91b578b613c327c5a83
ugurcankok <ugurcankok0@gmail.com>
2023-08-10 11:14:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# HTTP Requests Repository
+
+You can look at my work on HTTP requests in this repository.
+
+In this repository, you will find my works in ***src*** folder:
+
+* []()
---
+++ src/Exchange API/exchangeApi.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+api_url = "http://api.exchangeratesapi.io/v1/latest?access_key=API_KEY&base="
+
+exchange_type = input("Exchange Type: ")
+currency_received = input("Currency Received: ")
+amount = int(input(f"How much money {exchange_type}: "))
+
+result = requests.get(api_url + exchange_type)
+result = json.loads(result.text)
+
+print(f"{exchange_type} = {result['rates'][currency_received]} {currency_received}")
+print(f"{amount} {exchange_type} = {amount * result['rates'][currency_received]} {currency_received}")
